fb039ebc35e0bad5b49164f76a9e3b25a49dad5b
longpd <longpddev@gmail.com>
2022-10-02 16:05:50 +0700

    app support PWA

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,70 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
+const pwaOptions = {
+  mode: "development",
+  base: "/",
+  includeAssets: ["favicon.ico"],
+  manifest: {
+    name: "PWA Router",
+    short_name: "PWA Router",
+    theme_color: "#234",
+    icons: [
+      {
+        src: "logo-192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo-512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "logo-512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === "true",
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: "module",
+    navigateFallback: "index.html",
+  },
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === "true";
+const reload = process.env.RELOAD_SW === "true";
+const selfDestroying = process.env.SW_DESTROY === "true";
+
+if (process.env.SW === "true") {
+  pwaOptions.srcDir = "src";
+  pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+  pwaOptions.strategies = "injectManifest";
+  pwaOptions.manifest.name = "PWA Inject Manifest";
+  pwaOptions.manifest.short_name = "PWA Inject";
+}
+
+if (claims) pwaOptions.registerType = "autoUpdate";
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = "true";
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    plugins: [react()],
+    plugins: [react(), VitePWA(pwaOptions)],
     base: env.BASE_URL || "/",
     preview: {
       port: 3000,
