8d341f711a2706997ca9058cc5d3cf76357252f9
longpd <longpddev@gmail.com>
2022-11-05 21:07:12 +0700

    add lazy for huge

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,12 @@ import PopupWordDefinitions from "@components/PopupWordDefinitions";
 import Translation from "@components/Translation";
 import NavigateSite from "@components/NavigateSite/index";
 import SearchSite from "@components/SearchSite/index";
-import PopupShortCutDetail from "@components/PopupShortCutDetail/PopupShortCutDetail";
 const ToastContainer = React.lazy(() =>
   import("@components/Toast/ToastContainer")
+);
+
+const PopupShortCutDetail = React.lazy(() =>
+  import("@components/PopupShortCutDetail/PopupShortCutDetail")
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -24,7 +27,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <ToastContainer />
         </Suspense>
         <PopupWordDefinitions />
-        <PopupShortCutDetail />
+        <Suspense fallback={null}>
+          <PopupShortCutDetail />
+        </Suspense>
         <Translation />
         <NavigateSite />
         <SearchSite />
